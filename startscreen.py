a012775cd7ec1c3183a9656e9c3f40d1cd9945c9
LilyHaaseTV26 <lilyhaase128@gmail.com>
2025-02-13 11:45:00 -0700

    hey

--- startscreen.py
+++ startscreen.py
@@ -1,4 +1,4 @@
 from PyUI.Screen import Screen
 class StartScreen(Screen):
     def __init__(self, window):
-        super().__init__(window, (0,255,0)) ##use the parents constructor
+        super().__init__(window, (100,0,100)) ##use the parents constructor
