4374f536aae84e9579b01e02413213259ee04521
LilyHaaseTV26 <lilyhaase128@gmail.com>
2025-02-13 11:53:26 -0700

    blue<3

--- startscreen.py
+++ startscreen.py
@@ -1,4 +1,9 @@
 from PyUI.Screen import Screen
+from PyUI.PageElements import *
 class StartScreen(Screen):
     def __init__(self, window):
         super().__init__(window, (75,50,170)) ##use the parents constructor
+    def elementsToDisplay(self): #defines what elements will be on the page
+        self.elements = [
+            Label((50,50),100,100,"blue<3")
+    ]
