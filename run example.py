414c24ea636cf316f5cd67115187e59b7e96417d
LilyHaaseTV26 <lilyhaase128@gmail.com>
2025-02-14 11:52:21 -0700

    valentines

--- run example.py
+++ run example.py
@@ -1,6 +1,6 @@
 from PyUI.Window import Window
 ##import the custom screens you made---
-from startscreen import StartScreen
+from startscreen import StartScreen, SecondScreen, ThirdScreen
 ##-------------------------------------
 
 
@@ -8,17 +8,32 @@ window = Window("Example App", (100,0,100)) ##Create the window to work with
 
 ##Create Screen Objects for use------
 startScreen = StartScreen(window)
+secondScreen = SecondScreen(window)
+thirdScreen = ThirdScreen(window)
 ##-----------------------------------
 
 screen = startScreen ##set screen to be the starting screen
-
+screen2 = secondScreen
+screen3 = thirdScreen
+loopCounter = 1
 while True: ##Game loop
     ##Enter code here to handle changes between screens---
+    # if loopCounter == 1:
+    #     window.checkForInput(screen) #checks for inputs on the screen
+    #     screen.update() #updates the screen
+    #     window.update(screen) #updates the window to reflect the new screen
 
+    # if loopCounter ==2:
+    #     window.checkForInput(screen2) #checks for inputs on the screen
+    #     screen.update() #updates the screen
+    #     window.update(screen2) #updates the window to reflect the new screen
 
-
+    # if loopCounter == 3:
+    #     window.checkForInput(screen3) #checks for inputs on the screen
+    #     screen.update() #updates the screen
+    #     window.update(screen3) #updates the window to reflect the new screen
     ##----------------------------------------------------
-
+    loopCounter+=1
     window.checkForInput(screen) #checks for inputs on the screen
     screen.update() #updates the screen
     window.update(screen) #updates the window to reflect the new screen
--- startscreen.py
+++ startscreen.py
@@ -2,8 +2,24 @@ from PyUI.Screen import Screen
 from PyUI.PageElements import *
 class StartScreen(Screen):
     def __init__(self, window):
-        super().__init__(window, (75,50,170)) ##use the parents constructor
+        super().__init__(window, (255,220,220)) ##use the parents constructor
     def elementsToDisplay(self): #defines what elements will be on the page
         self.elements = [
-            Label((50,50),100,100,"blue<3")
+            Label((150,65),500,500,"<3 HAPPY VALENTINES DAY <3", 50, (255,255,255))
     ]
+
+class SecondScreen(Screen):
+    def __init__(self, window):
+        super().__init__(window, (225,0,0))
+    def elementsToDisplay(self):
+        self.elements = [
+            Label((150,65),500,500,"<3 HAPPY VALENTINES DAY <3", 50, (255,220,220))
+        ]
+
+class ThirdScreen(Screen):
+    def __init__(self, window):
+        super().__init__(window, (225,255,255))
+    def elementsToDisplay(self):
+        self.elements = [
+            Label((150,65),500,500,"<3 HAPPY VALENTINES DAY <3", 50, (255,0,0))
+        ]
