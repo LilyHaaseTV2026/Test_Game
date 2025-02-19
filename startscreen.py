00713e242b0bbd879ce169b98019e7323e1ea1e1
LilyHaaseTV26 <lilyhaase128@gmail.com>
2025-02-19 12:21:03 -0700

    rockpaperboom

--- startscreen.py
+++ startscreen.py
@@ -2,39 +2,50 @@ from PyUI.Screen import Screen
 from PyUI.PageElements import *
 class StartScreen(Screen):
     def __init__(self, window):
-        super().__init__(window, (255,220,220)) ##use the parents constructor
+        super().__init__(window, (83, 137, 181)) ##use the parents constructor
         self.state = {
-            "displayText": "Hello"
-        }
+            "displayText": " ",
+            "emoji": " "
+            }
 
     def elementsToDisplay(self): #defines what elements will be on the page
         self.elements = [
-            Label((150,65),500,500,self.state["displayText"], 50, (255,255,255)), Button((350, 150), 50, 50, '<3', (255,220,200), (255, 255, 255))
+            Label((350, 250), 100, 100, self.state["displayText"], 50, (255,255,255)), RockButton(),PaperButton(),ScissorsButton(), ShootButton()#,
+            #Label((350, 250), 100, 100, self.state["emoji"], 50, (255,255,255))
+
             #(topLeftLoc: Any, width: Any, height: Any, text: Any, textColorRGB: Any = (0, 0, 0), backColorRGB: Any = (255, 255, 255)) -> Button
             #(topLeftLoc: Any, width: Any, height: Any, text: Any, fontSize: int = 14, textColorRGB: Any = (0, 0, 0)) -> Label
             #"<3 HAPPY VALENTINES DAY <3"
+            #Button((350, 150), 50, 50, '<3', (255,220,200),(255, 255, 255))
+            #Label((150,65),500,500,self.state["displayText"], 50, (255,255,255)),
 
     ]
 
-class SecondScreen(Screen):
-    def __init__(self, window):
-        super().__init__(window, (225,0,0))
-    def elementsToDisplay(self):
-        self.elements = [
-            Label((150,65),500,500,"<3 HAPPY VALENTINES DAY <3", 50, (255,220,220))
-        ]
-
-class ThirdScreen(Screen):
-    def __init__(self, window):
-        super().__init__(window, (225,255,255))
-    def elementsToDisplay(self):
-        self.elements = [
-            Label((150,65),500,500,"<3 HAPPY VALENTINES DAY <3", 50, (255,0,0))
-        ]
-
-class HelloButton(Button):
+class RockButton(Button):
     def __init__(self):
-        super().__init__((200, 200), 100, 100, "Hello")
+        super().__init__((50, 400), 100, 100, "Rock", (83, 137, 181))
         
     def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
-        screen.state["displayText"] = "World" #modifies the state of the screen
+        screen.state["displayText"] = "Rock" #modifies the state of the screen
+
+class PaperButton(Button):
+    def __init__(self):
+        super().__init__((250, 400), 100, 100, "Paper", (83, 137, 181))
+        
+    def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
+        screen.state["displayText"] = "Paper" #modifies the state of the screen
+
+class ScissorsButton(Button):
+    def __init__(self):
+        super().__init__((450, 400), 100, 100, "Scissors", (83, 137, 181))
+        
+    def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
+        screen.state["displayText"] = "Scissors" #modifies the state of the screen
+
+class ShootButton(Button):
+    def __init__(self):
+        super().__init__((650, 400), 100, 100, "Shoot",(83, 137, 181))
+        
+    def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
+        screen.state["displayText"] = "Boom!!!" #modifies the state of the screen
+       # screen.state["emoji"] = #(/U0001F4A5)
