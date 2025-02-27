364487ea8412fb40e8aef896bec1c00d42b25274
LilyHaaseTV26 <lilyhaase128@gmail.com>
2025-02-27 11:53:44 -0700

    gameplay

---
+++ run example copy.py
@@ -0,0 +1,52 @@
+from PyUI.Window import Window
+import random
+##import the custom screens you made---
+from startscreen import StartScreen
+from winscreen import SecondScreen
+from losescreen import LoseScreen
+##-------------------------------------
+
+
+window = Window("Example App", (100,0,100)) ##Create the window to work with
+
+##-----------------------------------
+##Create Screen Objects for use------
+startScreen = StartScreen(window)
+secondScreen = SecondScreen(window)
+loseScreen = LoseScreen(window)
+
+##-----------------------------------
+
+screen = startScreen ##set screen to be the starting screen
+#screen = secondScreen
+
+#computer play
+valuesRPS = {'rock' : 1, 'paper' : 2, 'scissors' : 3}
+compChoice = random.randint(valuesRPS['rock'], valuesRPS['scissors'])
+if compChoice == 1:
+    print('rock')
+if compChoice == 2:
+    print('paper')
+if compChoice == 3:
+    print('scissors')
+print(compChoice)
+
+
+
+
+while True: ##Game loop
+    ##Enter code here to handle changes between screens---
+    if screen.state.get("moveTo") == "WIN":
+            screen = secondScreen
+            screen.state.get("moveTo") == " "
+    if screen.state.get("moveTo") == "WIN":
+            screen = startScreen
+            screen.state.get("moveTo") == " "
+    if screen.state.get("moveTo") == "LOSE":
+            screen = loseScreen
+            screen.state.get("moveTo") == " "
+    if screen.state.get("moveTo") == "LOSE":
+            screen = startScreen
+            screen.state.get("moveTo") == " "
+    window.checkForInput(screen) #checks for inputs on the screen
+    window.update(screen) #updates the window to reflect the new screen
--- startscreen.py
+++ startscreen.py
@@ -2,7 +2,7 @@ from PyUI.Screen import Screen
 from PyUI.PageElements import *
 class StartScreen(Screen):
     def __init__(self, window):
-        super().__init__(window, (83, 137, 181)) ##use the parents constructor
+        super().__init__(window, (124, 171, 247)) ##use the parents constructor
         self.state = {
             "displayText": " ",
             "moveTo": " ", 
@@ -11,10 +11,10 @@ class StartScreen(Screen):
 
     def elementsToDisplay(self): #defines what elements will be on the page
         self.elements = [
-            Label((350, 250), 100, 100, self.state["displayText"], 50, (255,255,255)), RockButton(),PaperButton(),ScissorsButton(), ShootButton(),
-            Image((75, 525), 50, 50, './imgs/rock no backround.png'),  Image((262.5, 525), 75, 37.5, './imgs/paper no backround.png'),  
-            Image((475, 525), 50, 50, './imgs/scisors no backround.png'),  Image((662.5, 525), 75, 37.5, './imgs/boom no backround.png'),
-            WinButton()
+            Label((350, 250), 100, 100, self.state["displayText"], 50, (255,255,255)), RockButton(),PaperButton(),ScissorsButton(), #ShootButton(),
+            Image((175, 525), 50, 50, './imgs/rock no backround.png'),  Image((362.5, 525), 75, 37.5, './imgs/paper no backround.png'),  
+            Image((575, 525), 50, 50, './imgs/scisors no backround.png'),  #Image((662.5, 525), 75, 37.5, './imgs/boom no backround.png'),
+            WinButton(), LoseButton()
         ]
 
         if self.state["imageMaybe"] != " ":
@@ -25,7 +25,7 @@ class StartScreen(Screen):
 
 class RockButton(Button):
     def __init__(self):
-        super().__init__((50, 400), 100, 100, "Rock", (83, 137, 181))
+        super().__init__((150, 400), 100, 100, "Rock", (124, 171, 247))
         
     def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
         screen.state["displayText"] = "Rock" #modifies the state of the screen
@@ -33,7 +33,7 @@ class RockButton(Button):
 
 class PaperButton(Button):
     def __init__(self):
-        super().__init__((250, 400), 100, 100, "Paper", (83, 137, 181))
+        super().__init__((350, 400), 100, 100, "Paper", (124, 171, 247))
         
     def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
         screen.state["displayText"] = "Paper" #modifies the state of the screen
@@ -41,15 +41,16 @@ class PaperButton(Button):
 
 class ScissorsButton(Button):
     def __init__(self):
-        super().__init__((450, 400), 100, 100, "Scissors", (83, 137, 181))
+        super().__init__((550, 400), 100, 100, "Scissors", (124, 171, 247))
         
     def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
         screen.state["displayText"] = "Scissors" #modifies the state of the screen
         screen.state['imageMaybe'] = './imgs/scisors no backround.png'
 
+
 class ShootButton(Button):
     def __init__(self):
-        super().__init__((650, 400), 100, 100, "Shoot",(83, 137, 181))
+        super().__init__((650, 400), 100, 100, "Shoot",(124, 171, 247))
         
     def onClick(self, screen): #override the onClick method to do our bidding, MUST TAKE SCREEN AS ARGUMENT
         screen.state["displayText"] = "Boom!!!" #modifies the state of the screen
@@ -60,3 +61,11 @@ class WinButton(Button):
         super().__init__((100, 100), 100, 100, "Win!")
     def onClick(self, screen):
        screen.state["moveTo"] = "WIN"
+
+class LoseButton(Button):
+    def __init__(self):
+        super().__init__((600, 100), 100, 100, "Lose:(")
+    def onClick(self, screen):
+       screen.state["moveTo"] = "LOSE"
+
+
